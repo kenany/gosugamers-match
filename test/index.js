4127e5bcffa7e37fbfd1645a1d1d1bdf2791f828
Kenan Yildirim <kenan@kenany.me>
2014-11-29 18:27:45 -0500

    Add LoL match page test
    Closes #2

--- test/index.js
+++ test/index.js
@@ -8,6 +8,10 @@ var url = 'http://www.gosugamers.net/counterstrike/tournaments/4767-fragbite-' +
           'masters-cs-go-season-3/1257-playoffs/4772-playoffs/matches/57218-' +
           'team-ldlc-vs-fnatic';
 
+var url2 = 'http://www.gosugamers.net/lol/tournaments/4731-2014-world-' +
+           'championship/1245-group-stage/4735-group-d/matches/55832-cloud-9-' +
+           'vs-alliance-lol';
+
 test('exports a function', function(t) {
   t.plan(1);
   t.ok(isFunction(gosugamersMatch));
@@ -29,5 +33,18 @@ test('parses match page', function(t) {
     t.equal(data.round2.away, 16);
     t.equal(data.round3.home, 19);
     t.equal(data.round3.away, 16);
+  });
+});
+
+test('parses league match page', function(t) {
+  t.plan(6);
+
+  gosugamersMatch(url2, function(error, data) {
+    t.ok(isPlainObject(data));
+    t.equal(data.opponent1, 'Cloud 9');
+    t.equal(data.opponent2, 'Alliance LoL');
+    t.equal(data.score1, 0);
+    t.equal(data.score2, 1);
+    t.equal(data.date, 1411808400);
   });
 });
